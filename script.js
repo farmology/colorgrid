2a78ab77ee719a1f673216a79e51bc4f9e5c4018
farmology <abkim03@gmail.com>
2021-10-19 15:25:17 -0700

    Added function to clear and populate square container with user choice of cell numbers

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
 const gridContainer = document.querySelector(".gridcontainer");
 const squareContainer = document.querySelector(".squarecontainer");
 const cell = document.createElement("div");
+const clearButton = document.querySelector(".clearbutton");
 cell.classList.add('cell');
-cell.textContent = "test";
+//cell.textContent = "test";
 
 for (let i = 0; i < 256; i++) {
     squareContainer.appendChild(cell.cloneNode(true));
 };
+
+squareContainer.addEventListener('mouseover', function(e) {
+    if (e.target.className === 'cell') {
+        console.log('Bling!');
+        e.target.classList.add('hover');
+    }
+
+});
+
+clearButton.addEventListener('click', () => {
+    squareContainer.innerHTML = "";
+    let gridNum = prompt('Grid number?');
+    let cellNum = gridNum * gridNum;
+    squareContainer.style.gridTemplateColumns = `repeat(${gridNum}, 1fr)`;
+    for (let i = 0; i < cellNum; i++) {
+        squareContainer.appendChild(cell.cloneNode(true));
+    };
+});
+
+
 
 // center a square in the center of the page
 // square is a flexbox? and the contents will be CSS Grid
